7baac8aed854238bb08fd41f882b86431a2616a4
Brogoming <kotag@outlook.com>
2022-05-07 19:40:53 -0500

    Installed a practice game loop

--- PlatFormer/src/main/Game.java
+++ PlatFormer/src/main/Game.java
@@ -1,14 +1,50 @@
 package main;
 
-public class Game {
+public class Game implements Runnable{
 	
 	private GameWindow gameWindow;
 	private GamePanel gamePanel;
+	private Thread gameThread; //a separate thread that is ran
+	private final int FPS_SET = 120; //max frames per second
 	
 	public Game() { //constructor - head method of the class
 		gamePanel = new GamePanel(); //needs to create the game panel before the window
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus(); //gets input focus
+		startGameLoop();
+		
+	}
+	
+	private void startGameLoop() { //starts the separate thread
+		gameThread = new Thread(this);
+		gameThread.start(); //starts the thread
+	}
+
+	@Override
+	public void run() { //runs a separate thread
+		
+		double timePFrame = 1000000000.0/FPS_SET; //how long each frame will last in nano-seconds
+		long lastFrame = System.nanoTime();
+		long now = System.nanoTime();
+		
+		int frames = 0;
+		long lastCheck = System.currentTimeMillis();
+		
+		while(true) { //an infinite loop that always runs
+			
+			now = System.nanoTime();
+			if(now - lastFrame >= timePFrame) { //if the last  time we had a frame if it is more or equal to the duration we want to display
+				gamePanel.repaint(); //repaint the surface again
+				lastFrame = now;
+				frames++; //adds the number of frames per second (when this repaints)
+			}
+			
+			if(System.currentTimeMillis() - lastCheck >= 1000) { //fps counter
+				lastCheck = System.currentTimeMillis();
+				System.out.println("FPS " + frames);
+				frames = 0; //resets the frames
+			}
+		}
 		
 	}
 	
--- PlatFormer/src/main/GamePanel.java
+++ PlatFormer/src/main/GamePanel.java
@@ -1,8 +1,10 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics; //new
 import java.awt.event.KeyEvent; //new
 import java.awt.event.KeyListener; //new
+import java.util.Random;
 
 import javax.swing.JPanel; //new
 
@@ -12,10 +14,15 @@ import Inputs.MouseInputs; //allows the panel to access MouseInputs
 public class GamePanel extends JPanel{
 	
 	private MouseInputs mouseInputs;
-	private int xDelta = 100, yDelta = 100; //changes the x and y value
-
+	private float xDelta = 100, yDelta = 100; //changes the x and y value
+	private float xDir = 1f, yDir = 1f; //the directions of the x and y coordinates
+	private int frames = 0; //number of frames
+	private long lastCheck = 0; //every 1000 milsec, the last time we were in the if statement
+	private Color color = new Color(150, 20, 90); //changes the color of the rectangle with red, green, blue (0-255)
+	private Random random;
+	
 	public GamePanel() {
-		
+		random = new Random();
 		mouseInputs = new MouseInputs(this);
 		addKeyListener(new KeyBoardInputs(this)); //looks(listens) for KeyBoardInputs	
 		addMouseListener(mouseInputs); //looks(listens) for MouseInputs	
@@ -24,7 +31,6 @@ public class GamePanel extends JPanel{
 	
 	public void changeXDelta(int value) { //method that changes the x value
 		this.xDelta += value;
-		repaint();
 	}
 	
 	public void changeYDelta(int value) { //method that changes the y value
@@ -35,15 +41,38 @@ public class GamePanel extends JPanel{
 	public void setRectPos(int x, int y) { //when in a new position we apply the new values to x and y delta 
 		this.xDelta = x;
 		this.yDelta = y;
-		repaint();
 	}
 	
 	public void paintComponent(Graphics g) { //graphics allows us to draw
 		
 		super.paintComponent(g); //calling the super class JPanel paintComponent
 		
-		//g.drawRect(xDelta, yDelta, 200, 50); //x, y, wide, tall. Only draws it, not filled
-		g.fillRect(xDelta, yDelta, 200, 50);
+		updateRectangle();
+		
+		g.setColor(color); //changes the color of the rectangle with red, green, blue
+		g.fillRect((int)xDelta, (int)yDelta, 200, 50); //x, y, wide, tall.
+		
+	}
+
+	private void updateRectangle() { //allows the rectangle to move by itself and change color
+		xDelta += xDir;
+		if(xDelta > 400 || xDelta < 0) { //reverses the direction if the x delta of the rectangle x > 400 or <0
+			xDir*=-1;
+			color = getRandomColor();
+		}
+		yDelta += yDir;
+		if(yDelta > 400 || yDelta < 0) { //reverses the direction if the x delta of the rectangle x > 400 or <0
+			yDir*=-1;
+			color = getRandomColor();
+		}
+	}
+
+	private Color getRandomColor() { //change color method
+		int r = random.nextInt(255); //red
+		int g = random.nextInt(255); //green
+		int b = random.nextInt(255); //blue
+		
+		return new Color (r, g, b);
 	}
 	
 }
